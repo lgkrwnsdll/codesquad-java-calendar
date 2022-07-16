096ef900252c81b8c082f95ba8d795226efdda7e
lgkrwnsdll <80141718+lgkrwnsdll@users.noreply.github.com>
2022-07-16 17:47:16 +0900

    Update calendar main()

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,5 +1,12 @@
 package calendar;
 
 public class Calendar {
-
+	public static void main(String[] args) {
+		System.out.println("일 월 화 수 목 금 토\r\n"
+				+ "--------------------\r\n"
+				+ " 1  2  3  4  5  6  7\r\n"
+				+ " 8  9 10 11 12 13 14\r\n"
+				+ "15 16 17 18 19 20 21\r\n"
+				+ "22 23 24 25 26 27 28");
+	}
 }
