bff8183bc89d19663b6ae5fbaf155f5f305f83ae
lgkrwnsdll <80141718+lgkrwnsdll@users.noreply.github.com>
2022-07-16 18:32:41 +0900

    Find last day of the month

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,12 +1,16 @@
 package calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 	public static void main(String[] args) {
-		System.out.println("일 월 화 수 목 금 토\r\n"
-				+ "--------------------\r\n"
-				+ " 1  2  3  4  5  6  7\r\n"
-				+ " 8  9 10 11 12 13 14\r\n"
-				+ "15 16 17 18 19 20 21\r\n"
-				+ "22 23 24 25 26 27 28");
+		int[] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("수를 입력하세요: ");
+		int month = scanner.nextInt();
+		
+		
+		System.out.printf("해당 월은 %d일로 구성됩니다.", days[month - 1]);
+		scanner.close();
 	}
 }
